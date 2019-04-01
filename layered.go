18b663797b25e01260a2777ccad8262cdf727ca9
Adriano <adriano.selaviles@gmail.com>
2019-03-31 23:07:24 -0700

    cert cache

--- layered.go
+++ layered.go
@@ -5,28 +5,56 @@ package certcache
 
 import (
 	"context"
+	"errors"
 
 	"golang.org/x/crypto/acme/autocert"
 )
 
-// LayeredCache is a recursive data type which
-// consists of a cache, and secondary layered cache
-// to search in the event of a cache miss
-// The type itself implements the autocert.Cache interface
+// LayeredCache is an implementation of the autocert.Cache interface
+// The behavior of the cache consists in checking itself for hits, and having
+// a fall back storage layer to search in the event of a cache miss
 type LayeredCache struct {
-	layer     autocert.Cache
-	nextLayer *LayeredCache
+	layer       autocert.Cache
+	nextLayer   *LayeredCache
+	writePolicy WritePolicy
 }
 
+// WritePolicy determines the order in which the layered cache executes Put
+type WritePolicy string
+
+const (
+	// PolicyWriteDeepFirst will write to caches starting from the last layer
+	// provided. This is the default behavior, as the more common use case
+	// will be for the last layer to be the most persistent e.g. DB call
+	PolicyWriteDeepFirst = WritePolicy("DEEP_FIRST")
+	// PolicyWriteShallowFirst will write to caches starting from the top,
+	// often least persistent, layer e.g. the process heap
+	PolicyWriteShallowFirst = WritePolicy("SHALLOW_FIRST")
+)
+
 // NewCache returns a new layered cache given autocert.Cache implementations
-// to be executed in-order on cache-miss events
+// this is the default constructor
 func NewCache(layers ...autocert.Cache) *LayeredCache {
 	if len(layers) == 0 {
 		return nil
 	}
 	return &LayeredCache{
-		layer:     layers[0],
-		nextLayer: NewCache(layers[1:]...),
+		layer:       layers[0],
+		nextLayer:   NewCache(layers[1:]...),
+		writePolicy: PolicyWriteDeepFirst,
+	}
+}
+
+// NewCacheWithPolicy returns a new layered cache and allows the user
+// to specify the write policy
+func NewCacheWithPolicy(wp WritePolicy, layers ...autocert.Cache) *LayeredCache {
+	if len(layers) == 0 {
+		return nil
+	}
+	return &LayeredCache{
+		layer:       layers[0],
+		nextLayer:   NewCacheWithPolicy(wp, layers[1:]...),
+		writePolicy: wp,
 	}
 }
 
@@ -46,11 +74,47 @@ func (c *LayeredCache) Get(ctx context.Context, key string) ([]byte, error) {
 // Underlying implementations may use any data storage format,
 // as long as the reverse operation, Get, results in the original data.
 func (c *LayeredCache) Put(ctx context.Context, key string, data []byte) error {
-	return nil
+	switch c.writePolicy {
+	case PolicyWriteDeepFirst:
+		if c.nextLayer != nil {
+			if err := c.nextLayer.Put(ctx, key, data); err != nil {
+				return err
+			}
+		}
+		return c.layer.Put(ctx, key, data)
+	case PolicyWriteShallowFirst:
+		if err := c.layer.Put(ctx, key, data); err != nil {
+			return err
+		}
+		if c.nextLayer != nil {
+			return c.nextLayer.Put(ctx, key, data)
+		}
+		return nil
+	default:
+		return errors.New("unrecognized write policy")
+	}
 }
 
 // Delete removes a certificate data from the cache under the specified key.
 // If there's no such key in the cache, Delete returns nil.
 func (c *LayeredCache) Delete(ctx context.Context, key string) error {
-	return nil
+	switch c.writePolicy {
+	case PolicyWriteDeepFirst:
+		if c.nextLayer != nil {
+			if err := c.nextLayer.Delete(ctx, key); err != nil {
+				return err
+			}
+		}
+		return c.layer.Delete(ctx, key)
+	case PolicyWriteShallowFirst:
+		if err := c.layer.Delete(ctx, key); err != nil {
+			return err
+		}
+		if c.nextLayer != nil {
+			return c.nextLayer.Delete(ctx, key)
+		}
+		return nil
+	default:
+		return errors.New("unrecognized write policy")
+	}
 }
